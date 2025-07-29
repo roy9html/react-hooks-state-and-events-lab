fd65cd24ea8e0cf633764bcb1275423e78ff31a3
roy9 <roymurimi527@gmail.com>
2025-07-29 23:12:40 +0300

    modify react

--- src/data/items.js
+++ src/data/items.js
@@ -1,9 +1,10 @@
 const items = [
   { id: 1, name: "Yogurt", category: "Dairy" },
-  { id: 2, name: "Pomegranate", category: "Produce" },
-  { id: 3, name: "Lettuce", category: "Produce" },
-  { id: 4, name: "String Cheese", category: "Dairy" },
-  { id: 5, name: "Cookies", category: "Dessert" },
+  { id: 2, name: "Apple", category: "Produce" },
+  { id: 3, name: "Ice Cream", category: "Dessert" },
+  { id: 4, name: "Broccoli", category: "Produce" },
+  { id: 5, name: "Milk", category: "Dairy" },
+  { id: 6, name: "Cake", category: "Dessert" },
 ];
 
 export default items;
